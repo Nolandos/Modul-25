fd7ec4236e94c6a3b8a3fb24d1fe108bf81a0555
Łukasz Nolka <nolkalukasz@gmail.com>
2019-08-08 21:42:00 +0200

    Add google authorization for aplication

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '987679733915-83n51k7n19pb71v4l4chmf4srahqebro.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'yW4s11Nzl6BpFp-cP05FtKyA',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+}
--- server.js
+++ server.js
@@ -1,14 +1,43 @@
 //Import Modules
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const config = require('./config');
+
+//Login session
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
 
 //Create app 
 const app = express();
+let googleProfile = {};
+
+//Passport configuration authorization request
+passport.use(new GoogleStrategy({
+    clientID: config.GOOGLE_CLIENT_ID,
+    clientSecret:config.GOOGLE_CLIENT_SECRET,
+    callbackURL: config.CALLBACK_URL
+},
+function(accessToken, refreshToken, profile, cb) {
+    googleProfile = {
+        id: profile.id,
+        displayName: profile.displayName
+    };
+    cb(null, profile);
+}
+));
 
 //Pug
 app.set('view engine', 'pug');
 app.set('views','./views');
 
 //Middlewares
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static('css'));
 app.use('/store', function(req, res, next){
     console.log('Jestem pośrednikiem przy żądaniu do /store');
@@ -17,34 +46,26 @@ app.use('/store', function(req, res, next){
 
 //ROUTES
 app.get('/', function (req, res) {
-    res.send('Witaj Wędrowcze!');
-});
-
-app.get('/store', function (req, res) {
-    res.send('To jest sklep');
-});
-
-app.get('/first-template', function(req, res){
-    res.render('first-template');
-});
-
-app.get('/login', function(req, res){
-    res.render('login', {
-        name: "Zadanie z puga",
-        style: "/style.css",
+    res.render('index', {
         url: "http://localhost:3000/auth/google",
-        method: "GET"
+        user: req.user
     });
 });
 
-app.get('/auth/google', function(req, res) {
-    res.render('auth', {
-        name: "Zalogowany",
-        login: req.query.login,
-        password: req.query.password,
-        style: "/auth.css"
-    });
+app.get('/logged', function(req, res){
+    res.render('logged', { user: googleProfile });
 });
+
+//Passport routes
+app.get('/auth/google',
+passport.authenticate('google', {
+scope : ['profile', 'email']
+}));
+app.get('/auth/google/callback',
+    passport.authenticate('google', {
+        successRedirect : '/logged',
+        failureRedirect: '/'
+    }));
 
 //Listening on PORT
 app.listen(3000);
