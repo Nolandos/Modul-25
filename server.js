98b2cdee3aaf1a0f2aa65b4780a76a619ec036d8
Łukasz Nolka <nolkalukasz@gmail.com>
2019-08-08 18:33:17 +0200

    Exercise with pugs templates

--- server.js
+++ server.js
@@ -1,43 +1,53 @@
 //Import Modules
-const experss = require('express');
-const bodyParser = require('body-parser');
-var fs = require('fs');
+const express = require('express');
 
 //Create app 
-const app = experss();
-let stringifyFile;
+const app = express();
+
+//Pug
+app.set('view engine', 'pug');
+app.set('views','./views');
 
 //Middlewares
-app.use(bodyParser.json());
+app.use(express.static('css'));
+app.use('/store', function(req, res, next){
+    console.log('Jestem pośrednikiem przy żądaniu do /store');
+    next();
+});
 
 //ROUTES
-app.get('/', (req, res) => {
-    res.send('Siema');
+app.get('/', function (req, res) {
+    res.send('Witaj Wędrowcze!');
 });
 
-app.get('/getNote', async (req, res) => {
-    try {
-        fs.readFile('./test.json', 'utf8', function(err, data) {
-            if (err) throw err;
-            stringifyFile = data
-            res.send(data);
-        });
-    } catch(err) {
-        res.send(err);
-    }
+app.get('/store', function (req, res) {
+    res.send('To jest sklep');
 });
 
-app.post('/getNote/:note', async (req, res) => {
-    try {
-        stringifyFile += req.params.note;
-        fs.writeFile('./test.json', stringifyFile, function(err) {
-            if (err) throw err;
-            console.log('file updated');
-        });
-    } catch(err) {
-        res.send(err);
-    }
-})
+app.get('/first-template', function(req, res){
+    res.render('first-template');
+});
+
+app.get('/login', function(req, res){
+    res.render('login', {
+        name: "Zadanie z puga",
+        style: "/style.css",
+        url: "http://localhost:3000/auth/google",
+        method: "GET"
+    });
+});
+
+app.get('/auth/google', function(req, res) {
+    res.render('auth', {
+        name: "Zalogowany",
+        login: req.query.login,
+        password: req.query.password,
+        style: "/auth.css"
+    });
+});
 
 //Listening on PORT
 app.listen(3000);
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});
